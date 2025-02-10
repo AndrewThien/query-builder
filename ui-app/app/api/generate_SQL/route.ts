05ad84266047618fdc422962239bf5ca834a3c8e
andrewthien <anretrithien@gmail.com>
2025-02-10 15:42:56 +0000

    add API to post JSON for SQL generation

---
+++ ui-app/app/api/generate_SQL/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest } from "next/server";
+import { db } from "@/lib/db";
+import { tableMetadata } from "@/lib/db/schema";
+import { TableMetaData } from "@/types/table";
+import { NextResponse } from "next/server";
+import axios from "axios";
+
+export async function POST(req: NextRequest) {
+  try {
+    const data = await req.json();
+
+    const response = await axios.post(
+      "http://localhost:7071/api/sqlGeneration",
+      data
+    );
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Error processing request" },
+      { status: 500 }
+    );
+  }
+}
