1d85b0e9f4b824da19f43559c36f740fa76ca262
andrewthien <anretrithien@gmail.com>
2025-02-13 11:50:57 +0000

    improve accordion

--- ui-app/components/AccordionFormik.tsx
+++ ui-app/components/AccordionFormik.tsx
@@ -42,7 +42,7 @@ export default function AccordionFormik({
     <Accordion type="single" collapsible>
       <AccordionItem value="item-1">
         <AccordionTrigger>{column_name}</AccordionTrigger>
-        <AccordionContent className="w-[255px]">
+        <AccordionContent className="w-[250px]">
           <Formik
             initialValues={{
               column_name: column_name,
@@ -66,58 +66,8 @@ export default function AccordionFormik({
           >
             {({ values, handleChange }) => (
               <Form>
-                <div className="flex flex-col gap-2 mt-1">
-                  <div className="flex flex-col gap-2">
-                    <div className="flex gap-3 items-center">
-                      <h1 className="font-semibold">Filter</h1>
-                      <div className="flex flex-col gap-2 border p-2 rounded-lg">
-                        <div className="flex gap-2 items-center">
-                          <label className="flex">
-                            Operator
-                            <Tooltips
-                              content="Supported operators are >,<,<=,>=,=,<>,!=,!<,!>,BETWEEN."
-                              link="https://learn.microsoft.com/en-us/sql/t-sql/language-elements/comparison-operators-transact-sql?view=sql-server-ver15"
-                            />
-                          </label>
-                          {/* TODO: change to Formik Select with the options depending on the data type */}
-                          <Input
-                            name={`operator`}
-                            onChange={handleChange}
-                            placeholder="Comparison Operator"
-                          />
-                        </div>
-                        <ErrorMessage
-                          name={`operator`}
-                          component="div"
-                          className="text-red-500"
-                        />
-
-                        {values.operator && (
-                          <div className="flex gap-2 items-center">
-                            <label className="flex">
-                              Value <Mandatory />
-                            </label>
-                            {data_type == "date" || data_type == "datetime" ? (
-                              <DatePickerField name="value" />
-                            ) : (
-                              <Input
-                                name={`value`}
-                                onChange={handleChange}
-                                placeholder="Right-side value of the filter"
-                                required
-                              />
-                            )}
-
-                            <ErrorMessage
-                              name={`value`}
-                              component="div"
-                              className="text-red-500"
-                            />
-                          </div>
-                        )}
-                      </div>
-                    </div>
-                  </div>
+                <div className="flex flex-col gap-2">
+                  {/* Reason field */}
                   <div>
                     <div className="flex gap-2 items-center">
                       <label className="font-semibold flex">
@@ -126,16 +76,64 @@ export default function AccordionFormik({
                       <Textarea
                         name={`reason`}
                         onChange={handleChange}
-                        placeholder="Reason for requesting this"
+                        placeholder="Reason for requesting data in this column."
                         required
                       />
                     </div>
                   </div>
+                  {/* Filter group */}
+                  <div className="flex gap-3 items-center">
+                    <h1 className="font-semibold flex">
+                      Filter{" "}
+                      <Tooltips content="Optional. If this is empty, all data in this column will be selected" />
+                    </h1>
+                    <div className="flex flex-col gap-2 border p-2 rounded-md">
+                      <div className="flex gap-2 items-center">
+                        <label className="flex">Operator</label>
+                        {/* TODO: change to Formik Select with the options depending on the data type */}
+                        <Input
+                          name={`operator`}
+                          onChange={handleChange}
+                          placeholder="Comparison Operator"
+                        />
+                      </div>
+                      <ErrorMessage
+                        name={`operator`}
+                        component="div"
+                        className="text-red-500"
+                      />
+
+                      {values.operator && (
+                        <div className="flex gap-2 items-center">
+                          <label className="flex">
+                            Value <Mandatory />
+                          </label>
+                          {data_type == "date" || data_type == "datetime" ? (
+                            <DatePickerField name="value" />
+                          ) : (
+                            <Input
+                              name={`value`}
+                              onChange={handleChange}
+                              placeholder="Right-side value of the filter"
+                              required
+                            />
+                          )}
+
+                          <ErrorMessage
+                            name={`value`}
+                            component="div"
+                            className="text-red-500"
+                          />
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                  {/* Submit button */}
                   <div className="flex justify-center">
                     <Button variant={"outline"} type="submit">
                       Add to Query <Plus className="size-4" />{" "}
                     </Button>
-                    <Tooltips content="If Filter is empty, all data in the column will be selected." />
+                    <Tooltips content="If filter is empty, all data in the column will be selected." />
                   </div>
                 </div>
               </Form>
